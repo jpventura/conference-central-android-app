4d8b8b24d3d97eca79d5d5f1ef48ead184e6bd99
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2014-12-29 07:56:25 -0200

    Lesson 4: Code for Creating Conferences.
    Summary:
    The following skeleton methods and classes were added for future implementation
    purpose:
      - com.google.devrel.training.conference.domain.Conference
      - com.google.devrel.training.conference.form.ConferenceForm
      - com.google.devrel.training.conference.form.ConferenceQueryForm
      - com.google.devrel.training.conference.spi.ConferenceApi.getProfileFromUser
      - com.google.devrel.training.conference.spi.ConferenceApi.createConference

--- backend/src/main/java/com/google/devrel/training/conference/domain/Profile.java
+++ backend/src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -15,9 +15,13 @@
 
 package com.google.devrel.training.conference.domain;
 
+import com.google.common.collect.ImmutableList;
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Profile class stores user's profile data.
@@ -45,6 +49,11 @@ public class Profile {
      * Options are defined as an Enum in ProfileForm
      */
     private TeeShirtSize teeShirtSize;
+
+    /**
+     * Keys of the conferences that this user registers to attend.
+     */
+    private List<String> conferenceKeysToAttend = new ArrayList<>(0);
 
     /**
      * Just making the default constructor private.
@@ -99,6 +108,14 @@ public class Profile {
     }
 
     /**
+     * Getter for conferenceIdsToAttend.
+     * @return an immutable copy of conferenceIdsToAttend.
+     */
+    public List<String> getConferenceKeysToAttend() {
+        return ImmutableList.copyOf(conferenceKeysToAttend);
+    }
+
+    /**
      * Update the Profile with the given displayName and teeShirtSize
      * @param displayName
      * @param teeShirtSize
@@ -110,6 +127,31 @@ public class Profile {
 
         if (null != teeShirtSize) {
             this.teeShirtSize = teeShirtSize;
+        }
+    }
+
+    /**
+     * Adds a ConferenceId to conferenceIdsToAttend.
+     *
+     * The method initConferenceIdsToAttend is not thread-safe, but we need a transaction for
+     * calling this method after all, so it is not a practical issue.
+     *
+     * @param conferenceKey a websafe String representation of the Conference Key.
+     */
+    public void addToConferenceKeysToAttend(String conferenceKey) {
+        conferenceKeysToAttend.add(conferenceKey);
+    }
+
+    /**
+     * Remove the conferenceId from conferenceIdsToAttend.
+     *
+     * @param conferenceKey a websafe String representation of the Conference Key.
+     */
+    public void unregisterFromConference(String conferenceKey) {
+        if (conferenceKeysToAttend.contains(conferenceKey)) {
+            conferenceKeysToAttend.remove(conferenceKey);
+        } else {
+            throw new IllegalArgumentException("Invalid conferenceKey: " + conferenceKey);
         }
     }
 }
