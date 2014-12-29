c9447e04f260f3118040dc91a7b1a6c9a5c30822
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2014-12-29 08:40:55 -0200

    Lesson 4: Save Conferences.

--- backend/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ backend/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -15,6 +15,7 @@
 
 package com.google.devrel.training.conference.spi;
 
+import static com.google.devrel.training.conference.service.OfyService.factory;
 import static com.google.devrel.training.conference.service.OfyService.ofy;
 
 import com.google.api.server.spi.config.Api;
@@ -169,31 +170,21 @@ public class ConferenceApi {
             throw new UnauthorizedException("Authorization required");
         }
 
-        // TODO (Lesson 4)
-        // Get the userId of the logged in User
+        String userId = user.getUserId();
 
-        // TODO (Lesson 4)
-        // Get the key for the User's Profile
+        Key<Profile> profileKey = Key.create(Profile.class, userId);
 
-        // TODO (Lesson 4)
-        // Allocate a key for the conference -- let App Engine allocate the ID
-        // Don't forget to include the parent Profile in the allocated ID
+        final Key<Conference> conferenceKey = factory().allocateId(profileKey, Conference.class);
 
-        // TODO (Lesson 4)
-        // Get the Conference Id from the Key
+        final long conferenceId = conferenceKey.getId();
 
-        // TODO (Lesson 4)
-        // Get the existing Profile entity for the current user if there is one
-        // Otherwise create a new Profile entity with default values
+        final Profile profile = getProfileFromUser(user);
 
-        // TODO (Lesson 4)
-        // Create a new Conference Entity, specifying the user's Profile entity
-        // as the parent of the conference
+        Conference conference = new Conference(conferenceId, userId, conferenceForm);
 
-        // TODO (Lesson 4)
-        // Save Conference and Profile Entities
+        ofy().save().entities(conference, profile).now();
 
-        return null;
+        return conference;
     }
 
     /**
