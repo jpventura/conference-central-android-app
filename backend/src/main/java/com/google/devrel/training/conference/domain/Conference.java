585758d556085b5a85c879ecdec3e821ca4c7b16
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2015-01-07 19:51:10 -0200

    Lesson 5: Using Memcache with Objectify.

--- backend/src/main/java/com/google/devrel/training/conference/domain/Conference.java
+++ backend/src/main/java/com/google/devrel/training/conference/domain/Conference.java
@@ -24,6 +24,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.devrel.training.conference.form.ConferenceForm;
 import com.google.gson.Gson;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
@@ -38,6 +39,7 @@ import java.util.List;
  * Conference class stores conference information.
  */
 @Entity
+@Cache
 public class Conference {
 
     @Index(IfNotDefault.class)
